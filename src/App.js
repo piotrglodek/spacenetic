ed03bbe0e1e0aacce595a6274ad6b66f355898e1
unknown <glodekpiotr@wp.pl>
2019-07-10 17:34:27 +0200

    created router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './view/Home';
 import WelcomePage from './view/WelcomePage';
+import Planets from './view/Planets';
 
 function App() {
  return (
   <Router>
    <Route exact path='/' component={Home} />
    <Route exact path='/welcome-page' component={WelcomePage} />
+   <Route exact path='/planets' component={Planets} />
   </Router>
  );
 }
