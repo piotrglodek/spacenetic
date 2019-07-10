4ded265106a80d8df507946a88301d77b1b9589c
unknown <glodekpiotr@wp.pl>
2019-07-10 17:34:17 +0200

    added welcome page

--- src/view/WelcomePage.js
+++ src/view/WelcomePage.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function WelcomePage() {
- return <div>Welcome page</div>;
+ return (
+  <div className='welcome'>
+   <h1 className='welcome__heading'>START YOUR JOURNEY</h1>
+   <Link to='/planets' className='welcome__circle welcome__circle--pulsing' />
+  </div>
+ );
 }
