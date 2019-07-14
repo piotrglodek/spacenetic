701cfaad8df272e6fbecf2a018a0aeca848d9046
unknown <glodekpiotr@wp.pl>
2019-07-14 16:09:59 +0200

    created pseudo carousel

--- src/view/Planets.js
+++ src/view/Planets.js
@@ -1,47 +1,126 @@
-import React from 'react';
+import React, { Component } from 'react';
 import arrow from '../assets/arrow.png';
+import { planetsData } from '../data';
 
-export default function Planets() {
- return (
-  <div className='planet'>
-   <header className='planet__header'>
-    <h2 className='planet__name'>Mercury</h2>
-    <button className='planet__btn'>
-     <img src={arrow} alt='arrow' className='arrow' />
-    </button>
-   </header>
-   <section className='planet__body'>
-    <p className='planet__info'>
-     mass: <span className='planet__info--highlighted'>0.33</span>
-    </p>
-    <p className='planet__info'>
-     diameter: <span className='planet__info--highlighted' />
-    </p>
-    <p className='planet__info'>
-     density: <span className='planet__info--highlighted' />
-    </p>
-    <p className='planet__info'>
-     gravity: <span className='planet__info--highlighted' />
-    </p>
-    <p className='planet__info'>
-     escape velocity: <span className='planet__info--highlighted' />
-    </p>
-    <p className='planet__info'>
-     rotation period: <span className='planet__info--highlighted' />
-    </p>
-    <p className='planet__info'>
-     length of day: <span className='planet__info--highlighted' />
-    </p>
-    <p className='planet__info'>
-     mean temperature: <span className='planet__info--highlighted' />
-    </p>
-    <p className='planet__info'>
-     number of moons: <span className='planet__info--highlighted' />
-    </p>
-    <p className='planet__info'>
-     has ring system: <span className='planet__info--highlighted' />
-    </p>
-   </section>
-  </div>
- );
+export default class Planets extends Component {
+ state = {
+  currentIndex: 0
+ };
+
+ goToNextPlanet = () => {
+  if (this.state.currentIndex === planetsData.length - 1) {
+   return this.setState({
+    currentIndex: 0
+   });
+  }
+  this.setState(prevState => ({
+   currentIndex: prevState.currentIndex + 1
+  }));
+ };
+
+ render() {
+  return (
+   <div
+    className='planet'
+    style={{
+     backgroundImage: `url(${planetsData[this.state.currentIndex].image})`
+    }}
+   >
+    <div className='planet__container'>
+     <button onClick={this.goToNextPlanet} className='planet__btn'>
+      <img src={arrow} alt='arrow' className='planet__arrow' />
+     </button>
+     <h2 className='planet__name'>
+      {planetsData[this.state.currentIndex].name}
+     </h2>
+     <p className='planet__data'>
+      Mass (10<sup>24</sup>kg):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].mass}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Diameter (km):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].diameter}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Density (kg/m<sup>3</sup>):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].density}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Gravity (m/s<sup>2</sup>):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].gravity}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Escape Velocity (km/s):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].escapeVelocity}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Rotation Period (hours):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].rotationPeriod}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Length of Day (hours):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].lengthOfDay}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Perihelion (10<sup>6</sup>km):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].perihelion}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Aphelion (10<sup>6</sup>km):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].aphelion}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Mean Temperature (C):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].meanTemperature}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Surface Pressure (bars):
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].surfacePressure}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Number of Moons:
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].numberOfMoons}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Ring System:
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].hasRingSystem ? 'yes' : 'no'}
+      </span>
+     </p>
+     <p className='planet__data'>
+      Global Magnetic Field:
+      <span className='planet__data--highlighted'>
+       {planetsData[this.state.currentIndex].hasGlobalMagneticField
+        ? 'yes'
+        : 'no'}
+      </span>
+     </p>
+    </div>
+   </div>
+  );
+ }
 }
